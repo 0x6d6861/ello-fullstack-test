85087cb3498aeb725f04dd35a88bf8658f87ad19
Heri Agape <13354804+0x6d6861@users.noreply.github.com>
2024-06-09 18:49:13 +0300

    student list ui improments

--- frontend/src/pages/protected/students/Index.tsx
+++ frontend/src/pages/protected/students/Index.tsx
@@ -4,10 +4,13 @@ import {
   Avatar,
   Box,
   Container,
+  Divider,
   List,
   ListItem,
   ListItemAvatar,
+  ListItemButton,
   ListItemText,
+  Paper,
   Typography,
 } from "@mui/material";
 import { Link } from "react-router-dom";
@@ -16,33 +19,59 @@ function Index(props) {
   const { data, isLoading } = api.useGetStudentsQuery(null);
 
   return (
-    <Container maxWidth="lg">
-      <Typography variant="h5" gutterBottom>
-        Student List
-      </Typography>
-      <List>
-        {data?.map((student) => (
-          <ListItem
-            to={`/students/${student.id}/books`}
-            component={Link}
-            key={student.id}
-          >
-            <ListItemAvatar>
-              <Avatar src={student.picture} alt={student.name} />
-            </ListItemAvatar>
-            <ListItemText
-              primary={student.name}
-              secondary={
-                <Box>
-                  <Typography variant="body2">
-                    Class: {student.class}
-                  </Typography>
-                </Box>
-              }
-            />
-          </ListItem>
-        ))}
-      </List>
+    <Container
+      maxWidth="lg"
+      sx={{
+        // backgroundColor: "red",
+        height: "100%",
+        display: "flex",
+        // justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      {/* <Typography variant="h5">Student List</Typography> */}
+      <Paper
+        variant="outlined"
+        sx={{
+          maxWidth: 400,
+          width: "100%",
+        }}
+      >
+        <List title="Student List" sx={{ width: "100%" }}>
+          {data?.map((student, index) => (
+            <>
+              <ListItemButton
+                to={`/students/${student.id}/books`}
+                component={Link}
+                key={student.id}
+              >
+                <ListItemAvatar>
+                  <Avatar
+                    sx={{
+                      width: 50,
+                      height: 50,
+                    }}
+                    src={student.picture}
+                    alt={student.name}
+                  />
+                </ListItemAvatar>
+                <ListItemText
+                  primary={student.name}
+                  secondary={
+                    <Box>
+                      <Typography variant="body2">
+                        Class: {student.class}
+                      </Typography>
+                    </Box>
+                  }
+                />
+              </ListItemButton>
+              {index !== data.length - 1 && <Divider />}
+            </>
+          ))}
+        </List>
+      </Paper>
     </Container>
   );
 }
--- frontend/src/store/api/data.ts
+++ frontend/src/store/api/data.ts
@@ -4,5 +4,11 @@ export const students = [
         name: 'James Doe',
         class: '5A',
         picture: '/students/pupils.webp'
+    },
+    {
+        id: 2,
+        name: 'Peter Kamau',
+        class: '3B',
+        picture: '/students/pupils.webp'
     }
 ]
